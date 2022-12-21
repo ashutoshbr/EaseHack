ce0133e7abe60dd9bfa546e4fc5d46bb792a10e2
ashutoshbr <ashutoshbr@proton.me>
2022-12-21 13:55:27 +0545

    🧪 Add a simple test

---
+++ tests/test_sign_data.py
@@ -0,0 +1,10 @@
+import unittest
+from utils.transcoder import sign_data
+
+# from utils.transcoder import sign_data
+
+
+class TestSignData(unittest.TestCase):
+    def test_not_none(self):
+        r = sign_data(b"Foo Bar")
+        return self.assertIsNotNone(r)
